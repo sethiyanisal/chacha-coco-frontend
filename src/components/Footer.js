130be2a1d1c13e439b2f8876b30e9efdc77be7c8
sethiyanisal <sethiya1098@gmail.com>
2023-06-10 20:32:51 +0530

    Changed the footer to a better responsive one

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,179 +1,81 @@
 import React from 'react'
-import { Link } from "react-router-dom";
 import Logo from '../images/Logo.png'
 
 const Footer = () => {
   return (
     <>
-    <footer className="p-4 bg-white sm:p-6 dark:bg-gray-800">
-        <div className="md:flex md:justify-between">
-            <div className="mt-20 ml-8 mb-6 md:mb-0">
-                <Link to="/" className="flex flex-col items-center">
-                <img src={Logo} className="h-24" alt="Ceylonese" />
-                <br />
-                <p className="mx-auto text-xl font-semibold dark:text-white">
-                    CHACHÅ COCO
-                </p>
-                </Link>
-            </div>
-            <div className="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-5 mt-8">
-                <div>
-                <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">
-                    Contact Us
-                </h2>
-                <ul className="text-gray-600 dark:text-gray-400">
-                    <li className="mb-4">
-                        Mihineth Associate [Pvt] Ltd<br/>
-                        37/1 Sirigala, Dambadeniya<br/>
-                        PC 60130<br/>
-                        Sri Lanka<br/>
-                    </li>
-                    <li className="mb-4">
-                    TP. +94777398274<br/>
-                    Fax. +94718801358<br/>
-                    WhatsApp. +94761431782<br/>
-                    </li>
-                    <li>
-                    chachacoco@mihineth.net
-                    </li>
-                </ul>
+    <footer className="bg-white dark:bg-gray-900">
+        <div className="container p-6 mx-auto">
+            <div className="lg:flex">
+                <div className="w-full flex justify-center lg:w-2/5">
+                    <div className="px-6">
+                        <a className='flex justify-center' href="/">
+                            <img className="w-auto h-10" src={Logo} alt=""/>
+                        </a>
+
+                        <p className="max-w-sm mt-2 text-gray-500 dark:text-gray-400">CHACHÅ COCO. Specialize in the production and export of premium quality coco peat products</p>
+
+                        <div className="flex justify-center mt-6 -mx-2">
+                            <a href="#"
+                                className="mx-2 text-gray-600 transition-colors duration-300 dark:text-gray-300 hover:text-blue-500 dark:hover:text-blue-400"
+                                aria-label="Reddit">
+                                <svg className="w-5 h-5 fill-current" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path
+                                        d="M12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.47715 2 12 2C17.5228 2 22 6.47715 22 12C21.9939 17.5203 17.5203 21.9939 12 22ZM6.807 10.543C6.20862 10.5433 5.67102 10.9088 5.45054 11.465C5.23006 12.0213 5.37133 12.6558 5.807 13.066C5.92217 13.1751 6.05463 13.2643 6.199 13.33C6.18644 13.4761 6.18644 13.6229 6.199 13.769C6.199 16.009 8.814 17.831 12.028 17.831C15.242 17.831 17.858 16.009 17.858 13.769C17.8696 13.6229 17.8696 13.4761 17.858 13.33C18.4649 13.0351 18.786 12.3585 18.6305 11.7019C18.475 11.0453 17.8847 10.5844 17.21 10.593H17.157C16.7988 10.6062 16.458 10.7512 16.2 11C15.0625 10.2265 13.7252 9.79927 12.35 9.77L13 6.65L15.138 7.1C15.1931 7.60706 15.621 7.99141 16.131 7.992C16.1674 7.99196 16.2038 7.98995 16.24 7.986C16.7702 7.93278 17.1655 7.47314 17.1389 6.94094C17.1122 6.40873 16.6729 5.991 16.14 5.991C16.1022 5.99191 16.0645 5.99491 16.027 6C15.71 6.03367 15.4281 6.21641 15.268 6.492L12.82 6C12.7983 5.99535 12.7762 5.993 12.754 5.993C12.6094 5.99472 12.4851 6.09583 12.454 6.237L11.706 9.71C10.3138 9.7297 8.95795 10.157 7.806 10.939C7.53601 10.6839 7.17843 10.5422 6.807 10.543ZM12.18 16.524C12.124 16.524 12.067 16.524 12.011 16.524C11.955 16.524 11.898 16.524 11.842 16.524C11.0121 16.5208 10.2054 16.2497 9.542 15.751C9.49626 15.6958 9.47445 15.6246 9.4814 15.5533C9.48834 15.482 9.52348 15.4163 9.579 15.371C9.62737 15.3318 9.68771 15.3102 9.75 15.31C9.81233 15.31 9.87275 15.3315 9.921 15.371C10.4816 15.7818 11.159 16.0022 11.854 16C11.9027 16 11.9513 16 12 16C12.059 16 12.119 16 12.178 16C12.864 16.0011 13.5329 15.7863 14.09 15.386C14.1427 15.3322 14.2147 15.302 14.29 15.302C14.3653 15.302 14.4373 15.3322 14.49 15.386C14.5985 15.4981 14.5962 15.6767 14.485 15.786V15.746C13.8213 16.2481 13.0123 16.5208 12.18 16.523V16.524ZM14.307 14.08H14.291L14.299 14.041C13.8591 14.011 13.4994 13.6789 13.4343 13.2429C13.3691 12.8068 13.6162 12.3842 14.028 12.2269C14.4399 12.0697 14.9058 12.2202 15.1478 12.5887C15.3899 12.9572 15.3429 13.4445 15.035 13.76C14.856 13.9554 14.6059 14.0707 14.341 14.08H14.306H14.307ZM9.67 14C9.11772 14 8.67 13.5523 8.67 13C8.67 12.4477 9.11772 12 9.67 12C10.2223 12 10.67 12.4477 10.67 13C10.67 13.5523 10.2223 14 9.67 14Z">
+                                    </path>
+                                </svg>
+                            </a>
+                        
+                            <a href="#"
+                                className="mx-2 text-gray-600 transition-colors duration-300 dark:text-gray-300 hover:text-blue-500 dark:hover:text-blue-400"
+                                aria-label="Facebook">
+                                <svg className="w-5 h-5 fill-current" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path
+                                        d="M2.00195 12.002C2.00312 16.9214 5.58036 21.1101 10.439 21.881V14.892H7.90195V12.002H10.442V9.80204C10.3284 8.75958 10.6845 7.72064 11.4136 6.96698C12.1427 6.21332 13.1693 5.82306 14.215 5.90204C14.9655 5.91417 15.7141 5.98101 16.455 6.10205V8.56104H15.191C14.7558 8.50405 14.3183 8.64777 14.0017 8.95171C13.6851 9.25566 13.5237 9.68693 13.563 10.124V12.002H16.334L15.891 14.893H13.563V21.881C18.8174 21.0506 22.502 16.2518 21.9475 10.9611C21.3929 5.67041 16.7932 1.73997 11.4808 2.01722C6.16831 2.29447 2.0028 6.68235 2.00195 12.002Z">
+                                    </path>
+                                </svg>
+                            </a>
+                        
+                        </div>
+                    </div>
                 </div>
-                <div>
-                <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">
-                    About Us
-                </h2>
-                <ul className="text-gray-600 dark:text-gray-400">
-                    <li className="mb-4">
-                    <Link to="/" className="hover:underline">
-                        Our story
-                    </Link>
-                    </li>
-                    <li>
-                    <Link to="/" className="hover:underline">
-                        Certificates
-                    </Link>
-                    </li>
-                </ul>
-                </div>
-                <div>
-                <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">
-                    Our Products
-                </h2>
-                <ul className="text-gray-600 dark:text-gray-400">
-                    <li className="mb-2">
-                    <Link to="/" className="hover:underline">
-                        ChaChå 10
-                    </Link>
-                    </li>
-                    <li>
-                    <Link to="/" className="hover:underline">
-                        ChaChå 8
-                    </Link>
-                    </li>
-                </ul>
-                </div>
-                <div>
-                <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">
-                    Follow us
-                </h2>
-                <ul className="text-gray-600 dark:text-gray-400">
-                    <li className="mb-4">
-                    <Link to="/" className="hover:underline ">
-                        Github
-                    </Link>
-                    </li>
-                    <li className="mb-4">
-                    <Link to="/" className="hover:underline">
-                        Discord
-                    </Link>
-                    </li>
-                </ul>
-                </div>
-                <div>
-                <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">
-                    Need Help?
-                </h2>
-                <ul className="text-gray-600 dark:text-gray-400">
-                    <li className="mb-4">
-                    <Link to="/" className="hover:underline">
-                        Privacy Policy
-                    </Link>
-                    </li>
-                    <li className="mb-4">
-                    <Link to="/refund" className="hover:underline">
-                        Terms &amp; Conditions
-                    </Link>
-                    </li>
-                    <li className="mb-4">
-                    <Link to="/support" className="hover:underline">
-                        Support ticket
-                    </Link>
-                    </li>
-                </ul>
+
+                <div className="mt-6 lg:mt-0 lg:flex-1">
+                    <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+                        <div>
+                            <h3 className="text-gray-700 uppercase dark:text-white">About</h3>
+                            <a href="#" className="block mt-2 text-sm text-gray-600 dark:text-gray-400 hover:underline">About Us</a>
+                            <a href="#" className="block mt-2 text-sm text-gray-600 dark:text-gray-400 hover:underline">Our Team</a>
+                        </div>
+
+                        <div>
+                            <h3 className="text-gray-700 uppercase dark:text-white">Our Products</h3>
+                            <a href="#" className="block mt-2 text-sm text-gray-600 dark:text-gray-400 hover:underline">Coco Fibre Pith</a>
+                            <a href="#" className="block mt-2 text-sm text-gray-600 dark:text-gray-400 hover:underline">Husk Chips</a>
+                            <a href="#" className="block mt-2 text-sm text-gray-600 dark:text-gray-400 hover:underline">Grow Blocks</a>
+                            <a href="#" className="block mt-2 text-sm text-gray-600 dark:text-gray-400 hover:underline">Grow Bricks</a>
+                            <a href="#" className="block mt-2 text-sm text-gray-600 dark:text-gray-400 hover:underline">Grow Bags</a>
+                        </div>
+
+                        <div>
+                            <h3 className="text-gray-700 uppercase dark:text-white">Certificates</h3>
+                            <a href="#" className="block mt-2 text-sm text-gray-600 dark:text-gray-400 hover:underline">Business Registration Certificate</a>
+                        </div>
+
+                        <div>
+                            <h3 className="text-gray-700 uppercase dark:text-white">Contact</h3>
+                            <span className="block mt-2 text-sm text-gray-600 dark:text-gray-400 hover:underline">+94 (77) 7398274</span>
+                            <span className="block mt-2 text-sm text-gray-600 dark:text-gray-400 hover:underline">chachacoco@mihineth.net</span>
+                            <span className="block mt-2 text-sm text-gray-600 dark:text-gray-400 hover:underline">Mihineth Associate [Pvt] Ltd 37/1 Sirigala, Dambadeniya PC 60130 Sri Lanka</span>
+                        </div>
+                    </div>
                 </div>
             </div>
-            </div>
-            <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-            <div className="sm:flex sm:items-center sm:justify-between">
-            <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
-                © 2023{" "}
-                <Link to="/" className="hover:underline">
-                CHACHÅ
-                COCO™
-                </Link>
-                . All Rights Reserved.
-            </span>
-            <div className="flex mt-4 space-x-6 sm:justify-center sm:mt-0">
-                <Link
-                to="/"
-                className="text-gray-500 hover:text-gray-900 dark:hover:text-white"
-                >
-                <svg
-                    className="w-5 h-5"
-                    fill="currentColor"
-                    viewBox="0 0 24 24"
-                    aria-hidden="true"
-                >
-                    <path
-                    fill-rule="evenodd"
-                    d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z"
-                    clip-rule="evenodd"
-                    />
-                </svg>
-                <span className="sr-only">Facebook page</span>
-                </Link>
-                <Link
-                to="/"
-                className="text-gray-500 hover:text-gray-900 dark:hover:text-white"
-                >
-                <svg
-                    className="w-5 h-5"
-                    fill="currentColor"
-                    viewBox="0 0 24 24"
-                    aria-hidden="true"
-                >
-                    <path
-                    fill-rule="evenodd"
-                    d="M12.315 2c2.43 0 2.784.013 3.808.06 1.064.049 1.791.218 2.427.465a4.902 4.902 0 011.772 1.153 4.902 4.902 0 011.153 1.772c.247.636.416 1.363.465 2.427.048 1.067.06 1.407.06 4.123v.08c0 2.643-.012 2.987-.06 4.043-.049 1.064-.218 1.791-.465 2.427a4.902 4.902 0 01-1.153 1.772 4.902 4.902 0 01-1.772 1.153c-.636.247-1.363.416-2.427.465-1.067.048-1.407.06-4.123.06h-.08c-2.643 0-2.987-.012-4.043-.06-1.064-.049-1.791-.218-2.427-.465a4.902 4.902 0 01-1.772-1.153 4.902 4.902 0 01-1.153-1.772c-.247-.636-.416-1.363-.465-2.427-.047-1.024-.06-1.379-.06-3.808v-.63c0-2.43.013-2.784.06-3.808.049-1.064.218-1.791.465-2.427a4.902 4.902 0 011.153-1.772A4.902 4.902 0 015.45 2.525c.636-.247 1.363-.416 2.427-.465C8.901 2.013 9.256 2 11.685 2h.63zm-.081 1.802h-.468c-2.456 0-2.784.011-3.807.058-.975.045-1.504.207-1.857.344-.467.182-.8.398-1.15.748-.35.35-.566.683-.748 1.15-.137.353-.3.882-.344 1.857-.047 1.023-.058 1.351-.058 3.807v.468c0 2.456.011 2.784.058 3.807.045.975.207 1.504.344 1.857.182.466.399.8.748 1.15.35.35.683.566 1.15.748.353.137.882.3 1.857.344 1.054.048 1.37.058 4.041.058h.08c2.597 0 2.917-.01 3.96-.058.976-.045 1.505-.207 1.858-.344.466-.182.8-.398 1.15-.748.35-.35.566-.683.748-1.15.137-.353.3-.882.344-1.857.048-1.055.058-1.37.058-4.041v-.08c0-2.597-.01-2.917-.058-3.96-.045-.976-.207-1.505-.344-1.858a3.097 3.097 0 00-.748-1.15 3.098 3.098 0 00-1.15-.748c-.353-.137-.882-.3-1.857-.344-1.023-.047-1.351-.058-3.807-.058zM12 6.865a5.135 5.135 0 110 10.27 5.135 5.135 0 010-10.27zm0 1.802a3.333 3.333 0 100 6.666 3.333 3.333 0 000-6.666zm5.338-3.205a1.2 1.2 0 110 2.4 1.2 1.2 0 010-2.4z"
-                    clip-rule="evenodd"
-                    />
-                </svg>
-                <span className="sr-only">Instagram page</span>
-                </Link>
-                <Link
-                to="/"
-                className="text-gray-500 hover:text-gray-900 dark:hover:text-white"
-                >
-                <svg
-                    className="w-5 h-5"
-                    fill="currentColor"
-                    viewBox="0 0 24 24"
-                    aria-hidden="true"
-                >
-                    <path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84" />
-                </svg>
-                <span className="sr-only">Twitter page</span>
-                </Link>
+
+            <hr className="h-px my-6 bg-gray-200 border-none dark:bg-gray-700"/>
+
+            <div>
+                <p className="text-center text-gray-500 dark:text-gray-400">© CHACHÅ COCO.MIHINETH ASSOCIATES (PVT) LTD - All rights reserved</p>
             </div>
         </div>
     </footer>
